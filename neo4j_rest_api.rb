51a742ce014cc4e050c3acbcefcb659b496d6bfc
Zhussupov Zhassulan <zhzhussupovkz@gmail.com>
2014-08-22 02:36:39 +0600

    add relationships methods

--- neo4j_rest_api.rb
+++ neo4j_rest_api.rb
@@ -66,6 +66,21 @@ class Neo4jRestApi
     result = JSON.parse(data)
   end
 
+  #send PUT request
+  def put_request url = nil, data = nil, headers = {}
+    post_data = data.to_json
+    url = @api_url + '/' + url
+    uri = URI.parse url
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    req = Net::HTTP::Put.new(uri.request_uri, initheader = headers)
+    req.body = post_data
+    res = http.request req
+    data = res.body
+    result = JSON.parse(data)
+  end
+
   #send DELETE request
   def delete_request url = nil, headers = {}
     url = @api_url + '/' + url
@@ -135,6 +150,101 @@ class Neo4jRestApi
       'Accept' => 'application/json; charset=UTF-8',
     }
     delete_request 'node/' + id, headers
+  end
+
+  #get relationship by id
+  def get_relationship_by_id id
+    headers = {
+      'Accept' => 'application/json; charset=UTF-8',
+    }
+    get_request 'relationship/' + id, headers
+  end
+
+  #create relationship
+  def create_relationship from_id, to_id, type, props
+    data = {
+      'to' => @api_url + 'node/' + to_id,
+      'type' => type,
+      'data' => props,
+    }
+
+    headers = {
+      'Accept' => 'application/json; charset=UTF-8',
+      'Content-Type' => 'application/json',
+    }
+
+    post_request 'node/' + from_id + '/relationships', data, headers
+  end
+
+  #delete relationship
+  def delete_relationship id
+    headers = {
+      'Accept' => 'application/json; charset=UTF-8',
+    }
+    delete_request 'relationship/' + id, headers
+  end
+
+  #get all properties on a relationship
+  def relationship_get_all_props id
+    headers = {
+      'Accept' => 'application/json; charset=UTF-8',
+    }
+    get_request 'relationship/' + id + '/properties', headers
+  end
+
+  #set all properties on a relationship
+  def relationship_set_props id, data
+    headers = {
+      'Accept' => 'application/json; charset=UTF-8',
+      'Content-Type' => 'application/json',
+    }
+
+    put_request 'relationship/' + id + '/properties', data, headers
+  end
+
+  #get single property on a relationship
+  def relationship_get_property id, name
+    headers = {
+      'Accept' => 'application/json; charset=UTF-8',
+    }
+
+    get_request 'relationship/' + id + '/properties/' + name, headers
+  end
+
+  #set single property on a relationship
+  def relationship_set_property id, name, value
+    headers = {
+      'Accept' => 'application/json; charset=UTF-8',
+    }
+
+    put_request 'relationship/' + id + '/properties/' + name, value, headers
+  end
+
+  #get all relationships
+  def get_all_relationships
+    headers = {
+      'Accept' => 'application/json; charset=UTF-8',
+    }
+
+    get_request 'relationships/all/', headers
+  end
+
+  #get incoming relationships
+  def get_in_relationships node_id
+    headers = {
+      'Accept' => 'application/json; charset=UTF-8',
+    }
+
+    get_request 'node/' + node_id + '/relationships/in', headers
+  end
+
+  #get outgoing relationships
+  def get_out_relationships node_id
+    headers = {
+      'Accept' => 'application/json; charset=UTF-8',
+    }
+
+    get_request 'node/' + node_id + '/relationships/out', headers
   end
 
 end
